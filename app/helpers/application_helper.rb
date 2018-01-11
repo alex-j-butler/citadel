89ab53ee495a26b30855e55041d5c554e501de37
Alex <a.jbutler019@gmail.com>
2018-01-11 15:15:31 +1000

    Added git revision print-out to admin bar.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -39,4 +39,12 @@ module ApplicationHelper
   def present_collection(collection, klass = nil)
     collection.map { |object| present(object, klass) }
   end
+
+  def git_revision
+    if File.exists?(File.join(Rails.root, "REVISION"))
+      File.open(File.join(Rails.root, "REVISION"), 'r') { |f| return f.gets.chomp }
+    else
+      `SHA1=$(git rev-parse HEAD 2> /dev/null); if [ $SHA1 ]; then echo $SHA1; else echo 'unknown'; fi`.chomp
+    end
+  end
 end
