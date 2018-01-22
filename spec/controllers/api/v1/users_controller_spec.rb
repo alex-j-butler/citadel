702533d1aa51aa5a4ceffeb3e9384817fc7671ad
Alex <a.jbutler019@gmail.com>
2018-01-22 17:32:37 +1000

    Added tests for users API.

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -15,6 +15,17 @@ describe API::V1::UsersController do
       end
     end
 
+    context 'when authenticated with invalid key' do
+      it 'fails with unauthorized error' do
+        request.headers['X-API-Key'] = 'an invalid key'
+        get :bans, params: { id: user.id }
+
+        expect(response).to have_http_status(:unauthorized)
+        expect(json['status']).to eq(401)
+        expect(json['message']).to eq('Unauthorized API key')
+      end
+    end
+
     context 'when authenticated' do
       context 'retrieving unbanned player' do
         it 'succeeds with 0 bans' do
@@ -37,6 +48,19 @@ describe API::V1::UsersController do
           expect(json['bans'].length).to eq(1)
           expect(json['bans'].first['type']).to eq('leagues')
         end
+      end
+
+      it 'succeeds with multiple bans' do
+        user.ban(:use, :leagues)
+        user.ban(:use, :forums)
+
+        request.headers['X-API-Key'] = key.key
+        get :bans, params: { id: user.id }
+
+        expect(response).to have_http_status(:success)
+        expect(json['bans'].length).to eq(2)
+        expect(json['bans'].any? { |hash| hash['type'] == 'leagues' }).to eq(true)
+        expect(json['bans'].any? { |hash| hash['type'] == 'forums' }).to eq(true)
       end
     end
   end
