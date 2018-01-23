57cea9a6f7217579b7a9646f994d1db922e018f0
Alex <a.jbutler019@gmail.com>
2018-01-23 20:05:24 +1000

    Updated home topics to be cached for 1 minute.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,9 +19,9 @@ class PagesController < ApplicationController
   def read_news_topic_config(config)
     limit = config['display'] || 3
 
-    @topic = Forums::Topic.find(config['id'])
-    @threads = @topic.threads.ordered.limit(limit)
-    @news_posts = @threads.map { |thread| [thread, thread.original_post] }.to_h
-    @more_threads = @topic.threads.limit(limit + 1).size > limit
+    @topic = Rails.cache.fetch('home_topic', expires_in: 1.minute) { Forums::Topic.find(config['id']) }
+    @threads = Rails.cache.fetch('home_threads', expires_in: 10.minute) { @topic.threads.ordered.limit(limit) }
+    @news_posts = Rails.cache.fetch('home_news_posts', expires_in: 10.minute) { @threads.map { |thread| [thread, thread.original_post] }.to_h }
+    @more_threads = Rails.cache.fetch('home_more_threads', expires_in: 10.minute) { @topic.threads.limit(limit + 1).size > limit }
   end
 end
