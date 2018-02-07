96605157cf6da8bf53408ed7e79057083467e91d
Alex <a.jbutler019@gmail.com>
2018-02-08 08:57:05 +1000

    Improved admin bar's integration with turbolinks.

--- app/assets/javascripts/admin_bar.js
+++ app/assets/javascripts/admin_bar.js
@@ -1,4 +1,5 @@
 var requestId = null;
+var cachedLabels = {};
 
 (function() {
 
@@ -14,52 +15,56 @@ var requestId = null;
 		}
 	}
 
-	updatePerformanceBar = function(results) {
+	updatePerformanceBar = function(results, firstLoad) {
 		for (var key in results.data) {
 			for (var label in results.data[key]) {
 				$("[data-defer-to=" + key + "-" + label + "]")
 					.text(results.data[key][label]);
+
+				// Cache results for seamless changes during turbolink navigation
+				cachedLabels[key + '_' + label] = { key: key, label: label, data: results.data[key][label] };
 			}
 		}
-
-		$("#admin-bar > .container")
-			.css({ opacity: 0 })
-			.animate({ opacity: 1 }, 50);
 	}
 
-	$(document).on('adminbar:preupdate', function() {
-		$("#admin-bar > .container")
-			.animate({ opacity: 0 }, 50);
+	$(document).on('adminbar:render', function(cached) {
+		if (cached) {
+			for (var key in cachedLabels) {
+				var l = cachedLabels[key];
+				$("[data-defer-to=" + l.key + "-" + l.label + "]")
+					.text(l.data);
+			}
+		}
 	});
 
-	$(document).on('adminbar:update', function() {
+	$(document).on('adminbar:update', function(firstLoad) {
 		return $.ajax('/admin_bar', {
 			data: {
 				request_id: getRequestId()
 			},
 			success: function(data, textStatus, xhr) {
-				return updatePerformanceBar(data);
+				return updatePerformanceBar(data, firstLoad);
 			},
 			error: function(xhr, textStatus, error) {}
 		});
 	});
 
-	$(document).on('page:change turbolinks:load', function() {
+	$(document).on('turbolinks:render', function() {
 		if (!adminBarEnabled()) { return; }
 
-		return $(this).trigger('adminbar:update');
+		return $(this).trigger('adminbar:render', true);
 	});
 
-	$(document).on('turbolinks:visit', function() {
+	$(document).on('turbolinks:load', function() {
 		if (!adminBarEnabled()) { return; }
 
-		return $(this).trigger('adminbar:preupdate')
+		return $(this).trigger('adminbar:update', false);
 	});
 
 	return $(function() {
 		if (!adminBarEnabled()) { return; }
 
-		$(document).trigger('adminbar:update');
+		$(document).trigger('adminbar:update', true);
 	});
 
 }())
