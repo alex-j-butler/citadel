27bf62882a89e50de7bc8aee3019d2f689e96d0e
Alex <a.jbutler019@gmail.com>
2018-02-07 18:17:01 +1000

    Fixed admin bar results route not requiring authentication.

--- app/controllers/admin_bar_controller.rb
+++ app/controllers/admin_bar_controller.rb
@@ -1,5 +1,14 @@
 class AdminBarController < ApplicationController
+  before_action :require_admin_bar
+
   def results
     render json: AdminBar.get(params[:request_id])
   end
+
+  private
+
+  def require_admin_bar
+    raise ActionController::RoutingError.new('Not Found') unless adminbar_enabled?
+  end
+
 end
