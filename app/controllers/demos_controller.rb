ee06cd82020c8faa5cf1076a31b33e03ed9bc78c
Alex <a.jbutler019@gmail.com>
2018-02-16 16:49:05 +1000

    Implement searching for demos.

--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -1,6 +1,7 @@
 class DemosController < ApplicationController
   def index
-    @demos = Demo.paginate(page: params[:page])
+    @demos = Demo.search(params[:q])
+                 .paginate(page: params[:page])
                  .order('created_at DESC')
                  .load
   end
--- app/models/demo.rb
+++ app/models/demo.rb
@@ -2,4 +2,13 @@ class Demo < ApplicationRecord
   has_many :players, class_name: 'Demo::Player'
 
   mount_uploader :demo, DemoUploader
+
+  scope :search, (lambda do |query|
+    return order(:id) if query.blank?
+
+    steam_id = SteamId.to_64(query)
+    query = Search.transform_query(query)
+
+    joins(:players).where('(demo_players.steam_id = ?) OR (demo_players.steam_name <-> ?) < 0.9', steam_id, query)
+  end)
 end
