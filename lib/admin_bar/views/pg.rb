a67cc5c20e93cb156e6d594eb805f4d370fed425
Alex <a.jbutler019@gmail.com>
2018-02-18 14:37:01 +1000

    Update pg admin bar view to display queries.

--- lib/admin_bar/views/pg.rb
+++ lib/admin_bar/views/pg.rb
@@ -4,6 +4,7 @@ require 'concurrent/atomics'
 module PGQueryTime
   module Instrumented
     def exec(*args)
+      ::PG::Connection.queries.update { |value| value << { method: :exec, sql: args[0] } }
       start = Time.now
       super(*args)
     ensure
@@ -13,6 +14,7 @@ module PGQueryTime
     end
 
     def async_exec(*args)
+      ::PG::Connection.queries.update { |value| value << { method: :async_exec, sql: args[0] } }
       start = Time.now
       super(*args)
     ensure
@@ -22,6 +24,7 @@ module PGQueryTime
     end
 
     def exec_prepared(*args)
+      ::PG::Connection.queries.update { |value| value << { method: :exec_prepared, statement_name: args[0] } }
       start = Time.now
       super(*args)
     ensure
@@ -36,9 +39,10 @@ class PG::Connection
   prepend PGQueryTime::Instrumented
 
   class << self
-    attr_accessor :query_time, :query_count
+    attr_accessor :query_time, :query_count, :queries
   end
 
+  self.queries = Concurrent::AtomicReference.new([])
   self.query_time = Concurrent::AtomicReference.new(0)
   self.query_count = Concurrent::AtomicReference.new(0)
 end
@@ -49,13 +53,23 @@ module AdminBar
       def setup_subscribers
         before_request do
           # Reset Postgres query data.
+          ::PG::Connection.queries.value = []
           ::PG::Connection.query_time.value = 0
           ::PG::Connection.query_count.value = 0
         end
       end
 
       def results
-        { calls: query_calls, duration: query_duration }
+        { queries: queries, raw_queries: raw_queries, calls: query_calls, duration: query_duration }
+      end
+
+      def raw_queries
+        ::PG::Connection.queries.value
+      end
+
+      def queries
+        q = ::PG::Connection.queries.value.select{ |val| val[:method] == :async_exec && val[:sql] }.map{ |val| val[:sql] }
+        q.group_by(&:to_s).map { |a| {query: a[0], count: a[1].count} }
       end
 
       def query_time
