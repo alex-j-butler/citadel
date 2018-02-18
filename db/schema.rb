42d48e9b884dd2f6de53f385c1a1445279b9b1b9
Alex <a.jbutler019@gmail.com>
2018-02-19 08:42:29 +1000

    Added 'processed' boolean to demos.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180216003838) do
+ActiveRecord::Schema.define(version: 20180218224037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -205,6 +205,7 @@ ActiveRecord::Schema.define(version: 20180216003838) do
     t.datetime "updated_at", null: false
     t.string "server_name"
     t.integer "duration"
+    t.boolean "processed", default: false
   end
 
   create_table "formats", id: :serial, force: :cascade do |t|
